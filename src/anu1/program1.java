6543d16344e4bbaba4690fe3ee69f923de3b154a
anusha <yenigandla.anusha10@gail.com>
2019-07-04 13:26:22 +0530

    This a commit4

--- src/anu1/program1.java
+++ src/anu1/program1.java
@@ -4,6 +4,9 @@ public class program1 {
 	public static void main(String args)
 	{
 		System.out.println("This is anusha");
-	}
+
+System.out.println("This is anusha");	
+System.out.print("Hi hellk")
+}
 
 }
